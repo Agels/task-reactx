ec4d08a2d0a4c07a0f2ad1d3071f4f2867a67f90
Agels <ardhianagilr@gmail.com>
2022-04-04 23:07:11 +0700

    app js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          task-react deploy
         </a>
       </header>
     </div>
