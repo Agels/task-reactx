e6ad8232bb76445a8e51c99877c34749265bcd5e
Agels <ardhianagilr@gmail.com>
2022-04-18 20:41:45 +0700

    npm test bug fixing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
- import Bootstrap from './component/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './component/register'
 function App() {
   return (
    <div className='App'>
-      {/* <Register /> */}
-      <Bootstrap />
+      <Register />
    </div>
   );
 }
